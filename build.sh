d09f84553522edf87b3c66124b9068175f51fca9
rsuntk <rsuntk@yukiprjkt.my.id>
2025-05-18 20:58:18 +0700

    ci: use defconfig as default configurations
    * All of sudden, it's compiles smack_lsm, which is
    * not used in Android.
    * So, fine, i'll use defconfig as default base.
    
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ CLANG_TRIPLE=aarch64-linux-gnu-
 "
 
 mkdir -p out
-make $(echo $BUILD_FLAGS) $DEFCONFIG
+make $(echo $BUILD_FLAGS) defconfig $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) $(echo $BUILD_FLAGS) Image.gz-dtb
